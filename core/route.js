1844312863b1873d2c779f247f3dc51232e02ede
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-05-24 16:58:04 +0300

    Controller -> Route | NEW: WhereIAm - get your loction pics, places

---
+++ core/route.js
@@ -0,0 +1,16 @@
+module.exports = (tg) => {
+	let ExchangeRate = require('./controller/exchange-rate')(tg);
+	let OtherwiseController = require('./controller/otherwise-controller')(tg);
+	let NavigationController = require('./controller/navigation-controller')(tg);
+	let StartContoller = require('./controller/start-controller')(tg);
+	let TrendingTweetsController = require('./controller/trending-tweets-controller')(tg);
+	let WhereIAmController = require('./controller/where-i-am-controller')(tg);
+
+	tg.router
+		.when(['/start'], 'StartContoller')
+		.when(['/exchange'], 'ExchangeRate')
+		.when(['/trendingtweets', '/twittertop'], 'TrendingTweetsController')
+		.when(['/whereiam'], 'WhereIAmController')
+		.otherwise('OtherwiseController');
+
+};
