02cdf686791e2a262a3f54212088ef57a2d2fbf9
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-05-25 17:35:23 +0300

    minor fixes

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 8080;
 
-// set the view engine to ejs
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-    
+    console.log('Our app is running on http://telegram-bot-app.herokuapp.com:' + port);
+
     let config = require('./config');
 	let tg = require('telegram-node-bot')(config.telegramApi.token);
 	let route = require('./core/route')(tg);
