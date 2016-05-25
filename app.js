449fe37ed116525579692b2f02ae2dd75ddcf7b7
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-05-25 17:26:25 +0300

    start application with dynamic port number

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 "use strict";
-let config = require('./config');
-let tg = require('telegram-node-bot')(config.telegramApi.token);
-let route = require('./core/route')(tg);
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+    
+    let config = require('./config');
+	let tg = require('telegram-node-bot')(config.telegramApi.token);
+	let route = require('./core/route')(tg);
+});
