9911db9c4ef5d3a8e1495eb8f6420ed7f6531ee2
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-06-08 14:11:21 +0300

    Remove instagram API

--- core/controller/where-i-am-controller.js
+++ core/controller/where-i-am-controller.js
@@ -19,25 +19,13 @@ module.exports = (tg) => {
 					$.runMenu({
 					    message: 'Choose:',
 					    options: {
-					        parse_mode: 'Markdown' // in options field you can pass some additional data, like parse_mode
-					    },
-					    'Auth': () => {
-					    	req.post({ 
-					    			url: 'https://api.instagram.com/oauth/authorize/?client_id=7697c93a292c49ba9c6b637587395fa3&redirect_uri=REDIRECT-URI&response_type=code',
-					    		}, (body, response, err) => {
-								console.error(arguments);
-							});
+					        parse_mode: 'Markdown'
 					    },
 					    'Show picture from this location': () => {
-					    	ig.location('location_id', function(err, result, remaining, limit) {
-					    		console.error(arguments);
-					    	});
 					    },
 					    'Show intresting places near me': () => {
-					    	
 					    },
 					    'Exit': () => {
-					    	
 					    }
 					});  
 				}
